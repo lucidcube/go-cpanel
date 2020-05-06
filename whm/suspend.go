ead393eb567258764ae1265d629fbdc49138d41f
Brooke Bryan <brooke@bajb.net>
2020-05-07 00:21:07 +0100

    Unsuspend

--- whm/suspend.go
+++ whm/suspend.go
@@ -6,7 +6,7 @@ import (
 	"net/url"
 )
 
-// SuspendUserResponse response from creation of a user session through WHMAPI
+// SuspendUserResponse response from suspending a user accout
 type SuspendUserResponse struct {
 	response.ExtendedBaseWhmApiResponse
 }
@@ -21,6 +21,24 @@ func (c *Connection) Suspend(reason string) (*SuspendUserResponse, error) {
 		return nil, err
 	}
 	resp := &SuspendUserResponse{}
+	err = json.Unmarshal(body, resp)
+	return resp, err
+}
+
+// UnSuspendUserResponse response from un-suspending a users account
+type UnSuspendUserResponse struct {
+	response.ExtendedBaseWhmApiResponse
+}
+
+// UnSuspend un-suspends a users account
+func (c *Connection) UnSuspend() (*UnSuspendUserResponse, error) {
+	params := url.Values{}
+	params.Add("user", c.user)
+	body, err := c.WHMCall("unsuspendacct", params)
+	if err != nil {
+		return nil, err
+	}
+	resp := &UnSuspendUserResponse{}
 	err = json.Unmarshal(body, resp)
 	return resp, err
 }
